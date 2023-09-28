df933a09fa55416021d8376d663c3c286f61efdf
Boyan Bejanov <bbejanov@gmail.com>
2023-09-28 17:10:39 -0400

    version 0.3.0

--- src/libdaec/config.h
+++ src/libdaec/config.h
@@ -1,11 +1,11 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define DE_VERSION "0.2.5"
-#define DE_VERNUM 0x0250
+#define DE_VERSION "0.3.0"
+#define DE_VERNUM 0x0300
 #define DE_VER_MAJOR 0
-#define DE_VER_MINOR 2
-#define DE_VER_REVISION 5
+#define DE_VER_MINOR 3
+#define DE_VER_REVISION 0
 #define DE_VER_SUBREVISION 0
 
 #endif
